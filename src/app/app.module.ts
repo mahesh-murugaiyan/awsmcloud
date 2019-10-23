af1be555e4dbeaaad29889d422d605da6df63aaa
mahesh-murugaiyan <mmaheshpmp@gmail.com>
2019-10-23 13:15:40 -0700

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,19 @@ import { HelloComponent } from './hello.component';
 
 import {DragDropModule} from '@angular/cdk/drag-drop';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatDividerModule} from '@angular/material';
 import {MatListModule} from '@angular/material/';
 import {MatExpansionModule} from '@angular/material/expansion'
-
+import {MatTabsModule} from '@angular/material/tabs';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, 
       DragDropModule,
         MatCardModule,MatRadioModule,MatDividerModule,MatListModule,MatRadioModule
-    ,MatExpansionModule
+    ,MatExpansionModule, MatTabsModule,BrowserAnimationsModule
   
   ],
   declarations: [ AppComponent, HelloComponent ],
